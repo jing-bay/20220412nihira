46a63ac13e68e9724be1e342796972e81e335bbc
jing-bay <figue@hotmail.co.jp>
2022-04-17 22:09:39 +0900

    test

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthorController extends Controller
 {
     public function index()
     {
-        $items = Author::Paginate(4);
-        return view('index', ['items' => $items]);
+        $user = Auth::user();
+        $items = Author::paginate(4);
+        $param = ['items' => $items, 'user' =>$user];
+        return view('index', $param);
     }
     public function find()
     {
@@ -74,5 +77,24 @@ class AuthorController extends Controller
         $noItems = Author::doesntHave('book')->get();
         $param = ['hasItems' => $hasItems, 'noItems' => $noItems];
         return view('author.index',$param);
+    }
+
+public function check(Request $request)
+    {
+    $text = ['text' => 'ログインして下さい。'];
+    return view('auth', $text);
+    }
+
+    public function checkUser(Request $request)
+    {
+    $email = $request->email;
+    $password = $request->password;
+    if (Auth::attempt(['email' => $email,
+            'password' => $password])) {
+        $text =   Auth::user()->name . 'さんがログインしました';
+    } else {
+        $text = 'ログインに失敗しました';
+    }
+    return view('auth', ['text' => $text]);
     }
 }
